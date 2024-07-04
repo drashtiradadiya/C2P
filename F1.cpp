413a67ebb1012366be9264e4f1607f249b724583
drashtiradadiya <drashti2004radadiya@gmail.com>
2024-07-04 12:41:58 +0530

    chANGE IN F1 FILE

---
+++ F1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void swap(int *x,int *y ){
+    int tamp;
+    tamp=*x;
+    *x=*y;
+    cout<<*x<<endl<<x<<endl;
+    *y=tamp; 
+     cout<<*y<<endl<<y;
+}
+
+int main(){
+    cout<<"call by Address"<<endl;
+    int a=10;
+    int b=20;
+    cout<<&a<<endl<<&b<<endl;
+    
+    swap(&a,&b);
+    return 0;
+}
